25738d2f9f4c866c824d9098425ba6cb4876c60e
nezbite <nesbite85@gmail.com>
2022-11-03 18:13:21 +0100

    added projectile explosion

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public float explosionRadius = 5f;
+    public float explosionPower = 100f;
+    Rigidbody rb;
+    bool exploded = false;
+
     void Start() {
-        GetComponent<Rigidbody>().velocity = (transform.up * 10);
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = (transform.up * 10);
+        StartCoroutine(DestroyProjectile());
     }
 
     void OnCollisionEnter(Collision collision) {
-        Debug.Log("Explosion");
+        if (!exploded) {
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+            foreach (Collider hit in colliders) {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                if (rb != null) {
+                    rb.AddExplosionForce(explosionPower, explosionPos, explosionRadius);
+                }
+            }
+            exploded = true;
+        }
+    }
+
+    // Coroutine to destroy the projectile after 30 seconds
+    IEnumerator DestroyProjectile() {
+        yield return new WaitForSeconds(30);
         Destroy(gameObject);
     }
 }
